1453a4e1aa4640332dcd0c786218daac789db96b
Marc-Cozar <marc.cozar@insbaixcamp.cat>
2022-03-04 18:01:53 +0100

    Relationships are important for us

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $table = 'review';
+
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
